fba7f9d5f854189694eca8e6d9844f67a3e0a135
gurbanigulati17 <gurbanigulati17@gmail.com>
2020-09-11 19:28:50 +0530

    Add project files.

---
+++ WebAPI_Practice/Controllers/MVCController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAPI_Practice.Controllers
+{
+    //We are making MVC Controller to read the data entered in WebAPI Controller
+
+    public class MVCController : Controller
+    {
+        // GET: MVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAPI_Practice/Controllers/WebAPIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_Practice.Controllers
+{
+    public class WebAPIController : ApiController
+    {
+
+
+        private static List<string> studentNames = new List<string> { "Guru" ,"Lisa", "Karen" };
+
+
+
+
+        //To retrieve values as it is .........................
+
+        [HttpGet]
+        public IEnumerable<string> GetNames()
+        {
+            return studentNames;
+        }
+
+        
+
+
+        //To retrive values based on index stored used id parameters..........
+        [HttpGet]
+        public string GetNamesById(int id)
+        {
+            return studentNames[id];
+        }
+    }
+}
